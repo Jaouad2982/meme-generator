95d4d6bb8a9bb28432ea334e34355c2461d62dee
jaouad <Jadsarouti@outlook.com>
2022-06-01 17:53:59 -0400

    Random image generator | troll | finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "../css/header.css"
-import trollface from "../images/troll-face.png"
 
 const Header = () => {
     return (
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,9 +1,27 @@
 import React from "react"
 import "../css/mainContent.css"
+import dataInfo from "../data/data"
 const MainContent = () => {
+    console.log(dataInfo)
+    const memes = dataInfo.data.memes;
+
+    const [site, setUrl] = React.useState("")
+    function handleUrlChange() {
+        const index = Math.floor(Math.random() * memes.length)
+
+        setUrl(
+            memes[index].url
+        )
+    }
+
+
     return (
         <div>
-            <button>Get a new Image</button>
+            <button onClick={handleUrlChange}>Get a new Image</button>
+            <div className="image-container">
+                <img src={site} alt="" />
+            </div>
+
         </div>
     )
 }
