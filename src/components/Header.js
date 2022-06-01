39dcac0e466b102a3ddbdf932ef8dd69f3520957
jaouad <Jadsarouti@outlook.com>
2022-06-01 17:32:41 -0400

    Styling | adding Main content | header components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../css/header.css"
+import trollface from "../images/troll-face.png"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h4>Meme Generator</h4>
+            <p>Get a new Image Troll</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../css/mainContent.css"
+const MainContent = () => {
+    return (
+        <div>
+            <button>Get a new Image</button>
+        </div>
+    )
+}
+
+export default MainContent
